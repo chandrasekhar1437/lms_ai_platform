97fd236a4ecd274e0afc6adabfefcb7076a33bec
Nunna chandrasekhar <chandrasekharnunna983@gmail.com>
2026-08-25 11:29:09 +0530

    Fix missing os import in db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,14 +1,14 @@
 ﻿import os
-from motor.motor_asyncio import AsyncIOMotorClient
+import motor.motor_asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # MongoDB Atlas Cloud Connection String
 MONGO_URL = os.getenv(
-    "MONGO_URL", 
+    "MONGO_URL",
     "mongodb+srv://chandu14372:ch123456@chandu-coder.7gppjmr.mongodb.net/?appName=chandu-coder"
 )
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 db = client.lms_database
